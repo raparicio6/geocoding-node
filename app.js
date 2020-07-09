4a058366c291664c73a269c10a39a433b382b421
rodrigosettle <rodrigo@settlenetwork.com>
2020-07-09 20:45:07 -0300

    project setup

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
-const swaggerUi = require('swagger-ui-express');
 const config = require('./config');
 const routes = require('./app/routes');
 const errors = require('./app/middlewares/errors');
 const logger = require('./app/logger');
-const documentation = require('./documentation');
 
 const DEFAULT_BODY_SIZE_LIMIT = 1024 * 1024 * 10;
 const DEFAULT_PARAMETER_LIMIT = 10000;
@@ -33,7 +31,6 @@ app.use(helmet());
 app.use(bodyParser.json(bodyParserJsonConfig()));
 app.use(bodyParser.urlencoded(bodyParserUrlencodedConfig()));
 app.use(expressRequestIdMiddleware());
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(documentation));
 
 if (!config.isTesting) app.use(expressMiddleware({ loggerFn: logger.info }));
 
